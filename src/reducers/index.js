161ae456cf4ad42ea461ddcc14bd49c067ca25ad
Shawn Salat <shawn.salat@gmail.com>
2018-08-12 21:48:33 -0700

    fix bug in combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,15 @@
-import loadingReducer from './loadingReducer'
-import imagesReducer from './imagesReducer'
-import errorReducer from './errorReducer'
+import loadingReducer from './loadingReducer';
+import imagesReducer from './imagesReducer';
+import errorReducer from './errorReducer';
 
 import {
-    combinedReducers
+    combineReducers
 } from 'redux';
 
-const rootReducer = combinedReducers({
+const rootReducer = combineReducers({
     isLoading: loadingReducer,
     images: imagesReducer,
     error: errorReducer,
 });
 
-export default rootReducer
+export default rootReducer;
--- src/store/index.js
+++ src/store/index.js
@@ -7,10 +7,10 @@ const configureStore = () => {
     const store = createStore(
         rootReducer,
         window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__(),
+        window.__REDUX_DEVTOOLS_EXTENSION__()
     );
     return store;
-}
+};
 
 
 export default configureStore;
